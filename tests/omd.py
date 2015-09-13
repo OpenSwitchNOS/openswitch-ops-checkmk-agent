43664c669b3ec782182d874ca71acb536b23e11a
Shad Ansari <shad.ansari@hp.com>
2015-09-13 00:08:48 +0000

    Treat OMD container as HalonHost nodetype
    Change-Id: I715f6798abce701f1fa5e397bdd171d0e4747a08
    Signed-off-by: Shad Ansari <shad.ansari@hp.com>

--- tests/omd.py
+++ tests/omd.py
@@ -6,7 +6,7 @@ OMD_DOCKER_IMAGE = 'openswitch/omd'
 
 class OmdSwitch (DockerNode, Switch):
     def __init__(self, name, image=OMD_DOCKER_IMAGE, **kwargs):
-        kwargs['nodetype'] = "OmdSwitch"
+        kwargs['nodetype'] = "HalonHost"
         super(OmdSwitch, self).__init__(name, image, **kwargs)
         self.inNamespace = True
 
