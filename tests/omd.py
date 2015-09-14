2edea63dec804608bf6fc7389c19a7e16aa8c3f0
Shad Ansari <shad.ansari@hp.com>
2015-09-14 09:01:35 +0000

    checkmk ft test script
    Change-Id: Ia312c8b5698c10f15c5c7160a014345177005129
    Signed-off-by: Shad Ansari <shad.ansari@hp.com>

--- tests/omd.py
+++ tests/omd.py
@@ -7,6 +7,7 @@ OMD_DOCKER_IMAGE = 'openswitch/omd'
 class OmdSwitch (DockerNode, Switch):
     def __init__(self, name, image=OMD_DOCKER_IMAGE, **kwargs):
         kwargs['nodetype'] = "HalonHost"
+        kwargs['init_cmd'] = DOCKER_DEFAULT_CMD
         super(OmdSwitch, self).__init__(name, image, **kwargs)
         self.inNamespace = True
 
---
+++ tests/test_checkmk_ft.py
@@ -0,0 +1,123 @@
+#
+# !/usr/bin/python
+#
+# Copyright (C) 2015 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+import sys
+import time
+import pytest
+import subprocess
+import time
+import re
+from halonvsi.docker import *
+from halonvsi.halon import *
+from halonutils.halonutil import *
+from omd import *
+
+NUM_OF_SWITCHES = 2
+NUM_HOSTS_PER_SWITCH = 0
+
+SWITCH = "s1"
+OMD_SERVER = "s2"
+OMD_IP = "9.0.0.1"
+SWITCH_IP = "9.0.0.2"
+NETMASK = "24"
+
+class myTopo(Topo):
+    def build (self, hsts=0, sws=NUM_OF_SWITCHES, **_opts):
+
+        self.hsts = hsts
+        self.sws = sws
+
+        self.addSwitch(SWITCH)
+        self.addSwitch(name = OMD_SERVER, cls = OmdSwitch, **self.sopts)
+        #self.addLink(SWITCH, OMD_SERVER)
+
+class checkmkTest (HalonTest):
+    def setupNet (self):
+        self.net = Mininet(topo=myTopo(hsts = NUM_HOSTS_PER_SWITCH,
+                                       sws = NUM_OF_SWITCHES,
+                                       hopts = self.getHostOpts(),
+                                       sopts = self.getSwitchOpts()),
+                                       switch = HalonSwitch,
+                                       host = HalonHost,
+                                       link = HalonLink,
+                                       controller = None,
+                                       build = True)
+
+    def configure (self):
+        info("\n########## Configuring IPs.. ##########\n")
+
+        for switch in self.net.switches:
+            if isinstance(switch, HalonSwitch):
+                switch.cmd("systemctl enable checkmk-agent.socket")
+                switch.cmd("systemctl restart sockets.target")
+                switch.cmdCLI("configure terminal")
+                switch.cmdCLI("interface 1")
+                switch.cmdCLI("no shutdown")
+                switch.cmdCLI("ip address %s/%s" % (SWITCH_IP, NETMASK)),
+                switch.cmdCLI("exit")
+
+    def checkmk_getIPs (self):
+        info("\n########## test 1 ##########\n")
+        for switch in self.net.switches:
+            result = switch.cmd("ifconfig eth0")
+            ipAddrs = re.findall(r'[0-9]+(?:\.[0-9]+){3}', result)
+            for ipAddr in ipAddrs:
+                if ipAddr != '0.0.0.0' and not re.match("255", ipAddr):
+                    break
+            if isinstance(switch, HalonSwitch):
+                self.switchIpAddr = ipAddr
+            elif isinstance(switch, OmdSwitch):
+                self.omdIpAddr = ipAddr
+
+        print self.switchIpAddr
+        print self.omdIpAddr
+
+    def checkmk_addHost(self):
+        info("\n########## add host ##########\n")
+        for switch in self.net.switches:
+            if isinstance(switch, OmdSwitch):
+                args = ['curl', """-d 'request={"hostname": "%s", "folder": "os/linux"}'""" % self.switchIpAddr, """\"http://127.0.0.1/default/check_mk/webapi.py?action=add_host&_username=auto&_secret=secretpassword\""""]
+                switch.cmd(args)
+
+class Test_checkmk_basic_setup:
+    def setup (self):
+        pass
+
+    def teardown (self):
+        pass
+
+    def setup_class (cls):
+        Test_checkmk_basic_setup.test_var = checkmkTest()
+
+    def teardown_class (cls):
+        Test_checkmk_basic_setup.test_var.net.stop()
+
+    def setup_method (self, method):
+        pass
+
+    def teardown_method (self, method):
+        pass
+
+    def __del__ (self):
+        del self.test_var
+
+    def test_run (self):
+        self.test_var.configure()
+        self.test_var.checkmk_getIPs()
+        self.test_var.checkmk_addHost()
